4328dfbe5516e3d3d67bd5e2b187b01f8f79d3fa
Dave Guymon <davidwguymon@gmail.com>
2020-03-22 13:43:56 -0600

    Adds edit and destroy actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,29 @@ class PostsController < ApplicationController
         end
     end
 
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        if @post.destroy
+            redirect_to root_path
+        else
+            render @post
+        end
+    end
+
 private
     def post_params
         params.require(:post).permit(:title, :content)
