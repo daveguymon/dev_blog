d51bc8a1acac82c8062219b42513b7f398d150b7
Dave Guymon <davidwguymon@gmail.com>
2020-03-29 22:31:20 -0600

    Restricts ability to view users to admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
+    before_action do 
+        redirect_to posts_path unless current_user && current_user.admin?
+    end
 
     def index
         @users = User.all
